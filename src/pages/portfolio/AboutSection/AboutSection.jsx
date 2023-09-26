0cea08e3bb34110e161ecec1d2e402aee21d42c2
DipakGohil007 <gohildipak106@gmail.com>
2023-09-26 21:13:21 +0530

    updated hover effect

--- src/pages/portfolio/AboutSection/AboutSection.jsx
+++ src/pages/portfolio/AboutSection/AboutSection.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AboutSection.scss";
 
 const AboutSection = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const shouldAddDelay = screenWidth < 576;
+
   return (
     <div className="about-section container">
-      <h2 className="title" data-aos="fade-up" data-aos-delay="900">
+      <h2
+        className="title"
+        data-aos="fade-up"
+        data-aos-delay={shouldAddDelay ? "900" : null}
+      >
         I'm a <span className="highlight">Frontend Developer</span>.
       </h2>
       {/* <p className="desc" data-aos="fade-up" data-aos-delay="150">Currently, I'm a Frontend Developer at Simform,</p> */}
